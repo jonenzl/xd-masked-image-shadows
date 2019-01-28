c365918fea55fcd6038085b197476bfb322309d2
Jonathan Ellis <thejonathanellis@gmail.com>
2019-01-29 12:12:44 +1300

    Add rgba colour for rectangle fill, begin exploring adding shadow

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /*
- * Sample plugin scaffolding for Adobe XD.
+ * Masked Image Shadows
+ * v0.0.1
  *
- * Visit http://adobexdplatform.com/ for API docs and more sample code.
+ * Jonathan Ellis and Aaron Ooi
  */
-
 
 var {Rectangle, Color} = require("scenegraph");
 
@@ -11,11 +11,19 @@ function myCommand(selection) {
     // Go to Plugins > Development > Developer Console to see this log output
     console.log("Plugin command is running!");
 
-    // Insert a red square at (0, 0) in the current artboard or group/container
+    // Insert a white square at (0, 0) in the current artboard or group/container
     var shape = new Rectangle();
     shape.width = 100;
     shape.height = 100;
-    shape.fill = new Color("#f00");
+    shape.fill = new Color({r:255, g:0, b:0, a:255});
+    /*shape.fill = new Color("#FFFFFF");*/
+    
+    // Add a drop shadow to the rectangle -- currently not working, trying to figure out their API
+    /*shape.shadowOffsetX = 0;
+    shape.shadowOffsetY = 10;
+    shape.shadowBlur = 20;
+    shape.shadowColor = new Color({r:0, g:0, b:0, a:40});*/
+    
     selection.insertionParent.addChild(shape);
 }
 
