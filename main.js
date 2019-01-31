cd9880e8744840c174933c8f4f4360f2807e5d6c
Jonathan Ellis <thejonathanellis@gmail.com>
2019-02-01 08:36:07 +1300

    Complete function for executing function on multiple Mask Groups at once

--- main.js
+++ main.js
@@ -13,6 +13,47 @@
 let {Color, Shadow, scenegraph} = require("scenegraph");
 let commands = require("commands");
 
+/*
+* Get all selected Mask Groups
+* For each Mask Group:
+* Duplicate original image and mask shape
+* Delete the original image
+* Set duplicated mask shape fill to white, add default drop shadow, and send backward
+*/
+function createDropShadow(selection) {
+    // Access selected Mask Group/s
+    let node = selection.items;
+    
+    for (let i = 0; i < node.length; i++) {
+        // Select each Mask Group node
+        selection.items = node[i];
+        let current = selection.items[0];
+        
+        // Duplicate and ungroup Mask Group
+        commands.duplicate();
+        commands.ungroup();
+        
+        // Delete the duplicated image
+        let unusedImage = selection.items[0]
+        unusedImage.removeFromParent();
+        
+        // Edit the duplicated mask shape -- rename "shadow", white fill, no stroke, add default drop shadow
+        let shadowBox = selection.items[1]
+        shadowBox.name = "Shadow";
+        shadowBox.fill = new Color({r:255, g:255, b:255, a:255});
+        shadowBox.stroke = null;
+        shadowBox.shadow = new Shadow(0, 3, 6, new Color({r:0, g:0, b:0, a:40}));
+        
+        // Send the shadow behind the original Mask Group, group together, use original Mask Group layer name
+        commands.sendBackward();
+        selection.items = [node[i], shadowBox];
+        commands.group();
+        let newLayerName = selection.items[0];
+        newLayerName.name = node[i].name;
+    }
+    
+    selection.items = null; 
+}
 
 /*
 * Get selected Mask Group
@@ -21,7 +62,7 @@ let commands = require("commands");
 * Set duplicated mask shape fill to white, add default drop shadow
 * Send shadow backward and group with original Mask Group
 */
-function createDropShadow(selection) {
+/*function createDropShadow(selection) {
     // Go to Plugins > Development > Developer Console to see this log output
     console.log("Plugin command is running!");
     
@@ -52,94 +93,22 @@ function createDropShadow(selection) {
     
     // Unselect objects
     selection.items = null;
-}
-
-
-/*
-* Get all selected Mask Groups
-* For each Mask Group:
-* Duplicate original image and mask shape
-* Delete the original image
-* Set duplicated mask shape fill to white, add default drop shadow, and send backward
-*/
-/*function createDropShadow(selection) {
-    let node = selection.items;
-    console.log(selection.items.length);
-    console.log(node);
-    
-    for (let i = 0; i < selection.items.length; i++) {
-        let current = selection.items[i];
-        console.log(current);
-        commands.duplicate();
-        commands.ungroup();
-        let unusedImage = selection.items[0]
-        unusedImage.removeFromParent();
-        let shadowBox = selection.items[1]
-        shadowBox.name = "Shadow";
-        shadowBox.fill = new Color({r:255, g:255, b:255, a:255});
-        shadowBox.stroke = null;
-        shadowBox.shadow = new Shadow(0, 3, 6, new Color({r:0, g:0, b:0, a:40}));
-        commands.sendBackward();
-    }
-    
-    node.forEach(function(element) {
-        let current = element;
-        console.log(current);
-        commands.duplicate();
-        commands.ungroup();
-        let unusedImage = selection.items[0]
-        unusedImage.removeFromParent();
-        let shadowBox = selection.items[1]
-        shadowBox.name = "Shadow";
-        shadowBox.fill = new Color({r:255, g:255, b:255, a:255});
-        shadowBox.stroke = null;
-        shadowBox.shadow = new Shadow(0, 3, 6, new Color({r:0, g:0, b:0, a:40}));
-        commands.sendBackward();
-    })
-    
-    selection.items = null;  
 }*/
 
-
-/*
-* Adds drop shadows to multiple selections (does not group)
-* For each selection - duplicate the initial object
-* Set initial object fill to white, add default drop shadow, and send backward
-*/
-/*function createDropShadow(selection) {
-    let items = selection.items;
-    items.forEach(function(element) {
-        commands.duplicate();
-        let clone = element;
-        clone.fill = new Color({r:255, g:255, b:255, a:255});
-        clone.shadow = new Shadow(0, 3, 6, new Color({r:0, g:0, b:0, a:40}));
-        selection.items = null;
-    });
-}*/
-
-
-/*
-* Add drop shadow to one selection and group
-* (does not work for multiple selections)
-* For each selection - duplicate the initial object
-* Set initial object fill to white, add default drop shadow, and send backward
-*/
-/*function createDropShadow(selection) {
-    let items = selection.items;
-    items.forEach(function(element) {
-        let initial = element;
-        commands.duplicate();
-        let clone = selection.items[0];
-        clone.fill = new Color({r:255, g:255, b:255, a:255});
-        clone.stroke = null;
-        clone.shadow = new Shadow(0, 3, 6, new Color({r:0, g:0, b:0, a:40}));
-        commands.sendBackward();
-        selection.items = [initial, clone];
-        commands.group();
-        selection.items = null;
-    });
-}*/
-
+/*node.forEach(function(element) {
+    let current = element;
+    console.log(current);
+    commands.duplicate();
+    commands.ungroup();
+    let unusedImage = selection.items[0]
+    unusedImage.removeFromParent();
+    let shadowBox = selection.items[1]
+    shadowBox.name = "Shadow";
+    shadowBox.fill = new Color({r:255, g:255, b:255, a:255});
+    shadowBox.stroke = null;
+    shadowBox.shadow = new Shadow(0, 3, 6, new Color({r:0, g:0, b:0, a:40}));
+    commands.sendBackward();
+})*/
 
 module.exports = {
     commands: {
