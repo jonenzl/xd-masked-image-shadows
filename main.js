f39d61a240e3bdc5fc44eb498fa5350d62d397cc
Jonathan Ellis <thejonathanellis@gmail.com>
2019-02-01 15:16:22 +1300

    Adjust function to meet Adobe XD Plugin Experience Guidelines, add sample confirm and alert modals

--- main.js
+++ main.js
@@ -12,6 +12,7 @@
 
 let {Color, Shadow, scenegraph} = require("scenegraph");
 let commands = require("commands");
+const { confirm, alert } = require("./lib/dialogs.js");
 
 /*
 * Get all selected Mask Groups
@@ -21,8 +22,10 @@ let commands = require("commands");
 * Set duplicated mask shape fill to white, add default drop shadow, and send backward
 */
 function createDropShadow(selection) {
-    // Access selected Mask Group/s
+    // Access selected Mask Group/s, record user's initial selection
+    let initialSelection = selection.items.map(i => i)
     let node = selection.items;
+    console.log(node);
     
     for (let i = 0; i < node.length; i++) {
         // Select each Mask Group node
@@ -46,72 +49,39 @@ function createDropShadow(selection) {
         
         // Send the shadow behind the original Mask Group, group together, use original Mask Group layer name
         commands.sendBackward();
-        selection.items = [node[i], shadowBox];
+        
+        // Group together, use original Mask Group layer name (does not follow Plugin Guidelines 2.2)
+        /*selection.items = [node[i], shadowBox];
         commands.group();
         let newLayerName = selection.items[0];
-        newLayerName.name = node[i].name;
+        newLayerName.name = node[i].name;*/
     }
     
-    selection.items = null; 
+    // Reset selection in the document to the user's initial selection before plugin execution
+    selection.items = initialSelection;
 }
 
-/*
-* Get selected Mask Group
-* Duplicate original image and mask shape
-* Delete the original image
-* Set duplicated mask shape fill to white, add default drop shadow
-* Send shadow backward and group with original Mask Group
-*/
-/*function createDropShadow(selection) {
-    // Go to Plugins > Development > Developer Console to see this log output
-    console.log("Plugin command is running!");
+async function showAlert() {
+    const feedback = await confirm("Masked Image Shadows",
+    "Smart filters are nondestructive and will preserve your original images.",
+    ["Cancel", "Create Shadow"]);
     
-    // Access selected Mask Group
-    let node = selection.items[0];
-    
-    // Duplicate and ungroup Mask Group
-    commands.duplicate();
-    commands.ungroup();
-    
-    // Delete the duplicated image
-    let unusedImage = selection.items[0]
-    unusedImage.removeFromParent();
-    
-    // Edit the duplicated mask shape -- rename "shadow", white fill, no stroke, add default drop shadow
-    let shadowBox = selection.items[1]
-    shadowBox.name = "Shadow";
-    shadowBox.fill = new Color({r:255, g:255, b:255, a:255});
-    shadowBox.stroke = null;
-    shadowBox.shadow = new Shadow(0, 3, 6, new Color({r:0, g:0, b:0, a:40}));
+    switch (feedback.which) {
+        case 0:
+            break;
+        case 1:
+            break;
+    }
+}
 
-    // Send the shadow behind the original Mask Group, group together with original Mask Group layer name
-    commands.sendBackward();
-    selection.items = [node, shadowBox];
-    commands.group();
-    let newLayerName = selection.items[0];
-    newLayerName.name = node.name;
-    
-    // Unselect objects
-    selection.items = null;
+/*async function showAlert() {
+    await alert("Incorrect selection",
+    "In order to function correctly, this plugin requires one or more Mask Groups to be selected.");  
 }*/
-
-/*node.forEach(function(element) {
-    let current = element;
-    console.log(current);
-    commands.duplicate();
-    commands.ungroup();
-    let unusedImage = selection.items[0]
-    unusedImage.removeFromParent();
-    let shadowBox = selection.items[1]
-    shadowBox.name = "Shadow";
-    shadowBox.fill = new Color({r:255, g:255, b:255, a:255});
-    shadowBox.stroke = null;
-    shadowBox.shadow = new Shadow(0, 3, 6, new Color({r:0, g:0, b:0, a:40}));
-    commands.sendBackward();
-})*/
 
 module.exports = {
     commands: {
-        createDropShadow: createDropShadow
+        createDropShadow: createDropShadow,
+        showAlert
     }
 };
